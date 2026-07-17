a9896d3e01e22c2f77c7d9a1cb085bcffced03e0
WKr1shna <krishnanegi772@gmail.com>
2026-07-18 05:12:00 +0530

    feat: refine navbar searchbar, allow underscores in username, add show/hide password options

--- models/validation.js
+++ models/validation.js
@@ -5,10 +5,10 @@ const loginschema= joi.object({
 })
 
 const signupschema= joi.object({
-    username:joi.string().alphanum().min(4).max(30).required().messages({
+    username:joi.string().pattern(/^[a-zA-Z0-9_]+$/).min(4).max(30).required().messages({
     'string.min':'Username must be atleast 4 characters long',
     'string.max':'Username cannot exceed 30 characters',
-    'string.alphanum':'Username must contain only letters and numbers',
+    'string.pattern.base':'Username must contain only letters, numbers, and underscores',
     'string.empty':'Username is required'}),
     email: joi.string().email().required().messages({'string.email':'Must be a valid email','string.empty':'Email is required'}),
     password: joi.string().min(8).required().messages({'string.min':'Must be a valid password (8 char long)','string.empty':'Password is required'})
